945926e95bf4d3dbfcec5ce49736f90910be7fb3
S-lewis98 <102757094+S-lewis98@users.noreply.github.com>
2022-12-11 00:14:23 -0600

    WORKING!!!

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
     <div class="row row-cols-1 row-cols-md-2 g-4">
         <CardComponent class="col"  v-for="show in tvshows" v-bind:key="show.id" :movie="show" @push-to-movies="pushToMovies" ></CardComponent>
     </div>
-    <h1 id="total-price">Shopping Cart</h1>
-    <table>
+    
+    <table  v-show = "movieselectedAray.length >0">
+      <h1 >Shopping Cart</h1>
   <tr>
     <th>Movie Name</th>
     <th>Child Tickets</th>
@@ -14,23 +15,26 @@
     <th>Subtotal</th>
     <th>Remove Button</th>
   </tr>
-  <tr v-for="TOTALPACKAGE in movieselectedAray" v-bind:key="TOTALPACKAGE.name " >
+  <!--Made it to where it will cycle through all the objects int the array that we got from our card components and in each row its simply calling the name and amount of tickets that was in that object. I used index here to show which object we were looking at and it helps later on with calling specific item in our negative buttons. -->
+  <tr v-for="(TOTALPACKAGE, index) in movieselectedAray" v-bind:key="TOTALPACKAGE.Name" >
     <td>{{TOTALPACKAGE.Name}}</td>
-    <td>{{TOTALPACKAGE.childTicketAmount}}</td>
-    <td>{{TOTALPACKAGE.adultTicketAmount}}</td>
-    <td>{{subtotal(TOTALPACKAGE)}}</td>
+    <td>${{TOTALPACKAGE.childTicketAmount}} X $10 <button @click="subtractchild(index)">-</button></td>
+    <td>{{TOTALPACKAGE.adultTicketAmount}} X $13<button @click="subtractadult(index)">-</button></td>
+    <td>${{subtotal(TOTALPACKAGE)}}</td>
     <td><button v-on:click="remove(TOTALPACKAGE)">Remove</button></td>
   </tr>
   <tr>
-    <td>{{ total }}</td>
+    <!--simple function call but passing the entire array to it so it calculates all objects in it-->
+    <td>Total: ${{ total(movieselectedAray) }}</td>
   </tr>
 </table>
+ <span id="total-price"></span>
   </div>
 </template>
 
 <script>  
 import 'bootstrap'
-import EventBus from './event-bus.js'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import CardComponent from './components/CardComponent.vue';
 import NavBar from './components/NavBar.vue'
@@ -53,20 +57,34 @@ export default {
   axios
   .get("https://api.themoviedb.org/3/movie/now_playing?api_key=0da26c065354dc35d436eebfbd585205&language=en-US&page=1")
   .then((response) => {
+    //the slice is showing us how many results we are getting out of our axios call
     this.tvshows = response.data.results.slice(0,12);
   })
   
-  this.$bus.$on('push-to-movies', this.pushToMovies)
-  this.$bus = EventBus;
+ 
 },
   methods: {
+    //this one was fairly easy to where it just deletes the object out of that array gets called in the - button too 
     remove(TOTALPACKAGE) {
       const index = this.movieselectedAray.indexOf(TOTALPACKAGE)
     this.movieselectedAray.splice(index, 1)
   },
+  // both of these subtract buttons are bassically the same for me. What i did was if it was 1 or higher you can press and it will take the amount that was storen under that ticket ammount in the totalpackage obj and subract one from it, and if both adult and child tickets = 0 it calls the remove function to take away the object listing. 
+  subtractchild(indie){
+    if (this.movieselectedAray[indie].childTicketAmount>=1){this.movieselectedAray[indie].childTicketAmount -= 1}
+    if (this.movieselectedAray[indie].childTicketAmount ==0 && this.movieselectedAray[indie].adultTicketAmount ==0) { this.remove(this.movieselectedAray[indie])
+    }
+  },
+  subtractadult(indie){
+    if (this.movieselectedAray[indie].adultTicketAmount>=1){this.movieselectedAray[indie].adultTicketAmount -= 1}
+    if (this.movieselectedAray[indie].childTicketAmount ==0 && this.movieselectedAray[indie].adultTicketAmount ==0) { this.remove(this.movieselectedAray[indie])
+    }
+  },
+  //just returning the subtotals easy math 
     subtotal(haha) { 
-  return (haha.adultTicketAmount * 13.00) + (haha.childTicketAmount * 10.00)}
-},
+  return (haha.adultTicketAmount * 13.00) + (haha.childTicketAmount * 10.00)
+}
+,// simply get the array and loops through all the objects adding them into child and adult sums, them multiplies them base off of our constants. 
     total(movies) {
   let Asum = 0;
   let Csum = 0;
@@ -75,17 +93,17 @@ export default {
     Asum += movies[i].adultTicketAmount
     Csum += movies[i].childTicketAmount
   }
-  var sum= (Asum* 13.00)+ (Csum*10);
+  var sum= (Asum* 13.00)+ (Csum * 10.00);
   return sum;
 },
-
-    pushToMovies(event, itemtopush)
+    // this is how we are getting the array to the main. 
+    pushToMovies(itemtopush)
     {
       this.movieselectedAray.push(itemtopush)
     }
     
   }
-  
+}
 </script>
 
 <style>
--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -2,11 +2,11 @@
     <div id="movieobj" class="card" style="width: 18rem;">
         <img :src=imgPath class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">{{movie.name}}</h5>
+            <h5 class="card-title">{{movie.title}}</h5>
             <p class="card-text">{{movie.overview}}</p>
-            <input type="number" class="form-control" :id="`adult${movie.name}`"  placeholder="0"> Number of Adult's Ticket
-            <input type="number" class="form-control" :id="`child${movie.name}`" placeholder="0"> Number of Children's Ticket
-            <button id="purchase-Tickets" v-on:click="addTickets(movie.name)"> Purchase tickets</button>
+            <input type="number" class="form-control" :id="`adult${movie.title}`"  placeholder="0"> Number of Adult's Ticket
+            <input type="number" class="form-control" :id="`child${movie.title}`" placeholder="0"> Number of Children's Ticket
+            <button id="purchase-Tickets" v-on:click="addTickets(movie.title)"> Purchase tickets</button>
         </div>
     </div>
 </template>
@@ -19,6 +19,7 @@
         {
 
             return {
+                //getting the image for the movie in the cards spot
                 imgPath:  "https://image.tmdb.org/t/p/w500/" + this.movie.poster_path ,
                 
                  
@@ -27,19 +28,23 @@
 
 
         methods: {
-            //this is when you click on the adult ticket button and adds the ticket to an array
+            //this is when you click on the adult ticket button and adds this var object with a few helpful items
+            //sending in the name of the movie to the object
             addTickets(moviename) {
             var TOTALPACKAGE = {
+            //making the name element of the object set to the movies name
             Name: moviename,
+            //making the ticket amounts equal to what I have the inputs
             adultTicketAmount: document.getElementById('adult'+moviename).value,
             childTicketAmount: document.getElementById('child'+moviename).value
             }
+            //this is how i saw you can send these event listners to the main
             this.$emit('push-to-movies', TOTALPACKAGE)
             let element= document.getElementById("total-price")
             window.scroll(0, element.offsetTop)
             }
            
-            // I dont know how this is supposed to send the whole object in and with with ticket amount
+           
 
         }
       
